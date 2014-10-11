0dd723c6102aa17ce47f2ac5a149fac5d678c138
perlancar (@pc-home) <perlancar@gmail.com>
2014-10-11 12:37:17 +0700

    Expand the docs, add function confirm()

--- lib/SHARYANTO/Text/Prompt.pm
+++ lib/SHARYANTO/Text/Prompt.pm
@@ -9,7 +9,7 @@ use warnings;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(prompt);
+our @EXPORT_OK = qw(prompt confirm);
 
 sub prompt {
     my ($text, $opts) = @_;
@@ -55,12 +55,26 @@ sub prompt {
     $answer;
 }
 
+sub confirm {
+    my ($text, $opts) = @_;
+
+    $text //= "Confirm (yes/no)?";
+    $opts //= {};
+
+    my $answer = prompt($text, {
+        required=>1, regex=>qr/\A(y|yes|n|no)\z/i, default=>$opts->{default}});
+    $answer =~ /\A(y|yes)\z/i ? 1:0;
+}
+
 1;
 # ABSTRACT: Prompt user question
 
 =head1 FUNCTIONS
 
-=head2 prompt($text, \%opts)
+=head2 prompt($text, \%opts) => val
+
+Display C<$text> and ask value from STDIN. Will re-ask if value is not valid.
+Return the chomp-ed value.
 
 Options:
 
@@ -68,13 +82,37 @@ Options:
 
 =item * var => \$var
 
-=item * required => \$var
+=item * required => bool
+
+If set to true then will require that value is not empty (zero-length).
 
 =item * default => VALUE
 
+Set default value.
+
 =item * regex => REGEX
 
+Validate using regex.
+
+
+=head2 confirm($text, \%opts) => bool
+
+Display C<$text> and ask for yes or no. Will return bool.
+
+Options:
+
+=item * default => bool
+
+Set default value.
+
 =back
+
+
+=head1 TODO
+
+Option to stty off (e.g. when prompting password).
+
+Localize confirm().
 
 
 =head1 SEE ALSO
